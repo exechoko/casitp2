dc004d697a16e47c1ebe1ee4a23379be2bd4c25f
Exequiel <49988126+exechoko@users.noreply.github.com>
2019-08-28 16:53:52 -0300

    Add files via upload

--- build-TP2-Desktop_Qt_5_13_0_GCC_64bit-Debug/moc_personaje.cpp
+++ build-TP2-Desktop_Qt_5_13_0_GCC_64bit-Debug/moc_personaje.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_personaje_t {
-    QByteArrayData data[1];
-    char stringdata0[10];
+    QByteArrayData data[6];
+    char stringdata0[67];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,10 +32,16 @@ struct qt_meta_stringdata_personaje_t {
     )
 static const qt_meta_stringdata_personaje_t qt_meta_stringdata_personaje = {
     {
-QT_MOC_LITERAL(0, 0, 9) // "personaje"
+QT_MOC_LITERAL(0, 0, 9), // "personaje"
+QT_MOC_LITERAL(1, 10, 13), // "keyPressEvent"
+QT_MOC_LITERAL(2, 24, 0), // ""
+QT_MOC_LITERAL(3, 25, 10), // "QKeyEvent*"
+QT_MOC_LITERAL(4, 36, 10), // "movimiento"
+QT_MOC_LITERAL(5, 47, 19) // "actualizar_posicion"
 
     },
-    "personaje"
+    "personaje\0keyPressEvent\0\0QKeyEvent*\0"
+    "movimiento\0actualizar_posicion"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,22 +51,35 @@ static const uint qt_meta_data_personaje[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   24,    2, 0x0a /* Public */,
+       5,    0,   27,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void personaje::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<personaje *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->keyPressEvent((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
+        case 1: _t->actualizar_posicion(); break;
+        default: ;
+        }
+    }
 }
 
 QT_INIT_METAOBJECT const QMetaObject personaje::staticMetaObject = { {
@@ -83,14 +102,25 @@ void *personaje::qt_metacast(const char *_clname)
     if (!_clname) return nullptr;
     if (!strcmp(_clname, qt_meta_stringdata_personaje.stringdata0))
         return static_cast<void*>(this);
-    if (!strcmp(_clname, "QGraphicsRectItem"))
-        return static_cast< QGraphicsRectItem*>(this);
+    if (!strcmp(_clname, "QGraphicsPixmapItem"))
+        return static_cast< QGraphicsPixmapItem*>(this);
     return QObject::qt_metacast(_clname);
 }
 
 int personaje::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
     return _id;
 }
 QT_WARNING_POP
